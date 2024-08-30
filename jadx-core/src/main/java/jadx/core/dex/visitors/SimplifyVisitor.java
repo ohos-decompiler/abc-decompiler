46121512fda1455ced488b84a5717208c3d999af
hx1997 <hx970923@gmail.com>
2024-08-31 02:01:01 +0800

    opt: eliminate consecutive "isfalse" and "istrue" in if conditions

--- jadx-core/src/main/java/jadx/core/dex/visitors/SimplifyVisitor.java
+++ jadx-core/src/main/java/jadx/core/dex/visitors/SimplifyVisitor.java
@@ -274,14 +274,40 @@ public class SimplifyVisitor extends AbstractVisitor {
 		InsnArg f = insn.getArg(0);
 		if (f.isInsnWrap()) {
 			InsnNode wi = ((InsnWrapArg) f).getWrapInsn();
-			if (wi.getType() == InsnType.CMP_L || wi.getType() == InsnType.CMP_G) {
-				if (insn.getArg(1).isZeroLiteral()) {
-					// insn.changeCondition(insn.getOp(), wi.getArg(0).duplicate(), wi.getArg(1).duplicate());
-					// InsnRemover.unbindInsn(mth, wi);
-				} else {
-					LOG.warn("TODO: cmp {}", insn);
+			// simplify "if (isfalse(istrue(xxx) == null ? 1 : 0))" into "if (xxx == true)"
+			if (wi.getType() != InsnType.INVOKE || !((InvokeNode) wi).getCallMth().getName().equals("isfalse")) {
+				return;
+			}
+
+			if (wi.getArg(0).isInsnWrap()) {
+				InsnNode ternary_insn = ((InsnWrapArg) wi.getArg(0)).getWrapInsn();  // a ? b : c
+				if (ternary_insn.getType() != InsnType.TERNARY) {
+					return;
+				}
+
+				IfCondition condition = ((TernaryInsn) ternary_insn).getCondition();
+				InsnArg condition_arg0_wrap = condition.getCompare().getInsn().getArg(0);
+				InsnArg condition_arg1_wrap = condition.getCompare().getInsn().getArg(1);
+				// check the ternary expression is comparing to null
+				if (!condition_arg1_wrap.isZeroLiteral()) {
+					return;
+				}
+				if (condition_arg0_wrap.isInsnWrap()) {
+					InsnNode istrue_arg = ((InsnWrapArg) condition_arg0_wrap).getWrapInsn();
+					if (istrue_arg.getType() == InsnType.INVOKE && ((InvokeNode) istrue_arg).getCallMth().getName().equals("istrue")) {
+						insn.changeCondition(insn.getOp(), istrue_arg.getArg(0).duplicate(), LiteralArg.lit(1, ArgType.BOOLEAN));
+						InsnRemover.unbindInsn(mth, insn);
+					}
 				}
 			}
+//			if (wi.getType() == InsnType.CMP_L || wi.getType() == InsnType.CMP_G) {
+//				if (insn.getArg(1).isZeroLiteral()) {
+//					// insn.changeCondition(insn.getOp(), wi.getArg(0).duplicate(), wi.getArg(1).duplicate());
+//					// InsnRemover.unbindInsn(mth, wi);
+//				} else {
+//					LOG.warn("TODO: cmp {}", insn);
+//				}
+//			}
 		}
 	}
 
